c14504adb513e158b08c702845042550d2201bf8
Julius Gerhardus <julius@gerhardus.com>
2025-03-25 15:21:35 +0100

    redid structure

---
+++ main.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', function () {
+    const canvas = document.getElementById("renderCanvas"); // Get the canvas element
+    const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
+    var createScene = function () {
+        // This creates a basic Babylon Scene object (non-mesh)
+        var scene = new BABYLON.Scene(engine);
+
+        BABYLON.MeshBuilder.CreateBox("box", {});
+
+        const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
+        camera.attachControl(canvas, true);
+        const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+
+        return scene;
+    };
+
+    const scene = createScene(); // Call the createScene function
+
+    // Register a render loop to repeatedly render the scene
+    engine.runRenderLoop(function () {
+        scene.render();
+    });
+
+    // Watch for browser/canvas resize events
+    window.addEventListener("resize", function () {
+        engine.resize();
+    });
+});
